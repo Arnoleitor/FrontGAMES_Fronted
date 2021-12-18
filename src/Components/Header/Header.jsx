235bafd788c44c207f1e9443f3dee64cf0df13c0
Arnold <arnold201991@gmail.com>
2021-12-18 14:06:53 +0100

    Header and logo add

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Header.scss';
+import logo from '../../Assets/Images/Logo.png';
+
+
+const Header = () => {
+    
+    
+    
+    return(
+    
+        <div className="designHeader">
+            <div>
+                <img id="logo" src={logo} alt="logo"/>
+            </div>
+            <div id="menu">
+               
+                    
+                
+            </div>
+        </div>
+    )
+
+};
+
+export default Header;
